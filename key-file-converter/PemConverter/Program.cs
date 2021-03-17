20095abae2922487890cdc9e3701463d16f5f9ad
Rick Rossberg <rick.rossberg@theobald-software.com>
2021-03-17 17:27:57 +0100

    key converter for pem to xml key file conversion

---
+++ key-file-converter/PemConverter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace PemConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Missing Argument. Use PemLoader.exe yourPubKey.pem");
+            }
+
+            string pemFileName = args[0];
+            RSA rsa = RSA.Create();
+            rsa.ImportFromPem(File.ReadAllText(pemFileName));
+            File.WriteAllText(
+                $"{pemFileName.Split('.')[0]}.xml",
+                rsa.ToXmlString(false));
+        }
+    }
+}
